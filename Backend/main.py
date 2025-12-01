5dcc8ed8f57a14f34bbb9a59f64e7451ea0bf9ea
GODFREY DATA SCIENTIST <godfreyimbindi@gmail.com>
2025-12-01 07:16:43 +0300

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -173,9 +173,8 @@ def get_prediction(request: PredictIn):
     return response
 
 
-if __name__ == "__main__":
-    import os
     import uvicorn
-
-    port = int(os.environ.get("PORT", 8000))  # Use PORT from environment if available
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    
+    if __name__ == "__main__":
+        # Only used when running locally
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
